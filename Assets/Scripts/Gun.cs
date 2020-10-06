085dc210e92c7d46e7fe07bd154506a1be8b1caf
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-06 16:13:54 -0400

    Chapter 7 - Added sound effects for shooting and upgraded shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,27 +9,39 @@ public class Gun : MonoBehaviour
 
     private AudioSource audioSource;
 
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
+
     void FireBullet()
     {
-        // 1
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
-        // 2
-        bullet.transform.position = launchPosition.position;
-        // 3
-        bullet.GetComponent<Rigidbody>().velocity =
-        transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
 
-        //Ref to audio component
-        audioSource = GetComponent<AudioSource>();
-
-        //Plays sound
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        // if the player has upgraded, shoot 2 bullets at an angle next to main bullet
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity =
+            (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity =
+            ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+        if(isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -47,5 +59,28 @@ public class Gun : MonoBehaviour
         {
             CancelInvoke("FireBullet");
         }
+        
+        /*
+        //if time passed is greated than time upgraded, upgrade turns off
+        currentTime += Time.deltaTime;
+        if (currentTime > upgradeTime && isUpgraded == true)
+        {
+            isUpgraded = false;
+        }
+        */
+    }
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    //Begins upgrade and timer
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
