04f35bd3386e6e584f3a1590b864caba968710db
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-06 13:19:53 -0400

    Chapter 6 - Updated ArenaIdle animation and ArenaWall script

---
+++ Assets/Scripts/ArenaWall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour
+{
+
+    private Animator arenaAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets arena object
+        GameObject arena = transform.parent.gameObject;
+        //References the arena's animator component
+        arenaAnimator = arena.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Set IsLowered to true when trigger is activated
+    void OnTriggerEnter(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", true);
+    }
+
+    //When trigger not activated, islowered is false (walls raise)
+    void OnTriggerExit(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", false);
+    }
+
+ 
+}
