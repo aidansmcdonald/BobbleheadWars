ffe2a1af32462bbaa0b3c00f5b598f7656c9edff
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-06 14:05:02 -0400

    Chapter 6 - Updated PlayerController script and SpaceMarine controller / animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
 
+    public Animator bodyAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,11 +35,13 @@ public class PlayerController : MonoBehaviour
         0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            // TODO
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+
+            bodyAnimator.SetBool("IsMoving", true);
         }
 
         RaycastHit hit;
