d231dc4e73794ee4f5bf1443e6017edb39480827
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-13 15:32:28 -0400

    Chapter 8 - Added alien heads falling to floor and dissapearing after death

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -16,6 +16,9 @@ public class Alien : MonoBehaviour
     //Unity custom event type
     public UnityEvent OnDestroy;
 
+    public Rigidbody head;
+    public bool isAlive = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,29 +30,46 @@ public class Alien : MonoBehaviour
     {
         if (target != null)
         {
-            //Checks to see if specific time has passed then updates path
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (isAlive)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                //Checks to see if specific time has passed then updates path
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
     }
     //Make collision the trigger to destroy the alien
     void OnTriggerEnter(Collider other)
     {
-        Die();
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        //if statement to only call die once
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
-    //Destroy the alien when it dies
+    //Destroy the alien and cleanup properties
     public void Die()
     {
-        Destroy(gameObject);
-        //Notifies GM of death
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+
         OnDestroy.Invoke();
-        //Removes the listener
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+
+        head.GetComponent<SelfDestruct>().Initiate();
+
+        Destroy(gameObject);
     }
 }
