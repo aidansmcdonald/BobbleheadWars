80c2ab18c66afbf27c6a4b763d6e3c8cf12d73db
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-20 10:52:48 -0400

    Chapter 8 - Added particle effect on Marine death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,10 +27,13 @@ public class PlayerController : MonoBehaviour
     public Rigidbody marineBody;
     private bool isDead = false;
 
+    private DeathParticles deathParticles;
+
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        deathParticles = gameObject.GetComponentInChildren<DeathParticles>();
     }
 
     // Update is called once per frame
@@ -131,6 +134,9 @@ public class PlayerController : MonoBehaviour
         head.transform.parent = null;
         head.useGravity = true;
         SoundManager.Instance.PlayOneShot(SoundManager.Instance.marineDeath);
+
+        deathParticles.Activate();
+
         Destroy(gameObject);
     }
 }
