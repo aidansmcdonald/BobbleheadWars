4821a7ad62423614bf0b7dbde3b79ddf1d778336
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-20 11:17:45 -0400

    Chapter 8 - Complete - Added elevator that appears after win

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,6 +43,8 @@ public class GameManager : MonoBehaviour
 
     public GameObject DeathFloor;
 
+    public Animator arenaAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -171,5 +173,18 @@ public class GameManager : MonoBehaviour
         aliensOnScreen -= 1;
         //Decreases aliens needed to win
         totalAliens -= 1;
+
+        //End game once aliens are killed
+        if (totalAliens == 0)
+        {
+            Invoke("endGame", 2.0f);
+        }
+    }
+
+    private void endGame()
+    {
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.
+        elevatorArrived);
+        arenaAnimator.SetTrigger("PlayerWon");
     }
 }
